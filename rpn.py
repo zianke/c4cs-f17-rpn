2efa69ed8e580a23403583cdb2edf555b5449f78
kezian <kezian@umich.edu>
2017-10-27 12:25:49 -0400

    finish rpn

--- rpn.py
+++ rpn.py
@@ -10,23 +10,30 @@ def calculate(arg):
             num1 = stack[-1]
             num2 = stack[-2]
             stack = stack[:-2]
-            stack.append(num1 + num2)
+            stack.append(num2 + num1)
         elif x == '-':
             num1 = stack[-1]
             num2 = stack[-2]
             stack = stack[:-2]
-            stack.append(num1 - num2)
+            stack.append(num2 - num1)
         elif x == '*':
             num1 = stack[-1]
             num2 = stack[-2]
             stack = stack[:-2]
-            stack.append(num1 * num2)
+            stack.append(num2 * num1)
+        elif x == '/':
+            num1 = stack[-1]
+            num2 = stack[-2]
+            stack = stack[:-2]
+            stack.append(num2 / num1)
         else:
             pass
     if len(stack) == 1:
         print(stack[0])
+        return stack[0]
     else:
         print("Error: Malformed expression")
+        return "Error: Malformed expression"
 
 
 def main():
--- test_rpn.py
+++ test_rpn.py
@@ -1,7 +1,24 @@
 import unittest
 import rpn
 
+
 class TestBasics(unittest.TestCase):
     def test_add(self):
         result = rpn.calculate("1 1 +")
-        self.assertEqual(2,result)
+        self.assertEqual(2, result)
+
+    def test_subtract(self):
+        result = rpn.calculate("3 2 -")
+        self.assertEqual(1, result)
+
+    def test_multiply(self):
+        result = rpn.calculate("3 4 *")
+        self.assertEqual(12, result)
+
+    def test_divide(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
+
+    def test_malformed_input(self):
+        result = rpn.calculate("1 2 3 +")
+        self.assertEqual("Error: Malformed expression",result)
