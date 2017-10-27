897ff4ea5cd2402aee0ddf3aa677ad4a19491972
kezian <kezian@umich.edu>
2017-10-27 12:16:20 -0400

    finish + - *

--- rpn.py
+++ rpn.py
@@ -1,9 +1,38 @@
+import numbers
+
+
 def calculate(arg):
-	pass
+    stack = list()
+    for x in arg:
+        if x.isdigit():
+            stack.append(float(x))
+        elif x == '+':
+            num1 = stack[-1]
+            num2 = stack[-2]
+            stack = stack[:-2]
+            stack.append(num1 + num2)
+        elif x == '-':
+            num1 = stack[-1]
+            num2 = stack[-2]
+            stack = stack[:-2]
+            stack.append(num1 - num2)
+        elif x == '*':
+            num1 = stack[-1]
+            num2 = stack[-2]
+            stack = stack[:-2]
+            stack.append(num1 * num2)
+        else:
+            pass
+    if len(stack) == 1:
+        print(stack[0])
+    else:
+        print("Error: Malformed expression")
+
 
 def main():
-	while True:
-		calculate(input("rpn calc> "))
+    while True:
+        calculate(input("rpn calc> "))
+
 
 if __name__ == '__main__':
-	main()
+    main()
