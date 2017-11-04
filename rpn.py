991922bc6a7043e28bb3056a84720c128128eacd
kezian <kezian@umich.edu>
2017-11-04 01:02:51 -0400

    update files

--- rpn.py
+++ rpn.py
@@ -1,42 +1,36 @@
-def calculate(arg):
-    stack = list()
-    for x in arg:
-        if x.isdigit():
-            stack.append(float(x))
-        elif x == '+':
-            num1 = stack[-1]
-            num2 = stack[-2]
-            stack = stack[:-2]
-            stack.append(num2 + num1)
-        elif x == '-':
-            num1 = stack[-1]
-            num2 = stack[-2]
-            stack = stack[:-2]
-            stack.append(num2 - num1)
-        elif x == '*':
-            num1 = stack[-1]
-            num2 = stack[-2]
-            stack = stack[:-2]
-            stack.append(num2 * num1)
-        elif x == '/':
-            num1 = stack[-1]
-            num2 = stack[-2]
-            stack = stack[:-2]
-            stack.append(num2 / num1)
-        else:
-            pass
-    if len(stack) == 1:
-        print(stack[0])
-        return stack[0]
-    else:
-        print("Error: Malformed expression")
-        return "Error: Malformed expression"
+#!/usr/bin/env python3
 
+import operator
+
+
+operators = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,
+}
+
+def calculate(myarg):
+    stack = list()
+    for token in myarg.split():
+        try:
+            token = int(token)
+            stack.append(token)
+        except ValueError:
+            function = operators[token]
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = function(arg1, arg2)
+            stack.append(result)
+        print(stack)
+    if len(stack) != 1:
+        raise TypeError("Too many parameters")
+    return stack.pop()
 
 def main():
     while True:
-        calculate(input("rpn calc> "))
-
+        result = calculate(input("rpn calc> "))
+        print("Result: ", result)
 
 if __name__ == '__main__':
     main()
